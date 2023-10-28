ce6969f048c1acf56161da2836c996ee3beabd80
Jesus Alejandro Galaz Icedo <jesus.galaz@softtek.com>
2023-10-27 22:12:38 -0700

    agrege el campo active en cada una de las tablas para el borrado logico

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,42 +1,44 @@
--- Tabla de productos
+-- Table of products
 CREATE TABLE products (
     id INT PRIMARY KEY IDENTITY,
-    name NVARCHAR(100),
-    description NVARCHAR(255),
-    presentation NVARCHAR(50),
-    price DECIMAL(18, 2),
-    supplierId INT FOREIGN KEY REFERENCES suppliers(id),
-    createdAt DATETIME DEFAULT GETDATE(),
-    updatedAt DATETIME DEFAULT GETDATE(),
+    name NVARCHAR(100), -- Name of the product
+    description NVARCHAR(255), -- Description of the product
+    presentation NVARCHAR(50), -- Presentation of the product
+    price DECIMAL(18, 2), -- Price of the product
+    supplier_id INT FOREIGN KEY REFERENCES suppliers(id), -- ID of the supplier
+    created_at DATETIME DEFAULT GETDATE(), -- Date of registration
+    updated_at DATETIME DEFAULT GETDATE(), -- Date of last update
+    active BIT DEFAULT 1 -- Active flag for soft delete
 );
 
-
--- Tabla de proveedores
+-- Table of suppliers
 CREATE TABLE suppliers (
     id INT PRIMARY KEY IDENTITY,
-    name NVARCHAR(100),
-    brand NVARCHAR(100),
-    createdAt DATETIME DEFAULT GETDATE(),
-    updatedAt DATETIME DEFAULT GETDATE(),
+    name NVARCHAR(100), -- Name of the supplier
+    brand NVARCHAR(100), -- Brand of the supplier
+    created_at DATETIME DEFAULT GETDATE(), -- Date of registration
+    updated_at DATETIME DEFAULT GETDATE(), -- Date of last update
+    active BIT DEFAULT 1 -- Active flag for soft delete
 );
 
-
--- Tabla de vendedores
+-- Table of vendors
 CREATE TABLE vendors (
     id INT PRIMARY KEY IDENTITY,
-    name NVARCHAR(100),
-    age INT,
-    gender CHAR(1),
-    createdAt DATETIME DEFAULT GETDATE(),
-    
-    updatedAt DATETIME DEFAULT GETDATE(),
+    name NVARCHAR(100), -- Name of the vendor
+    age INT, -- Age of the vendor
+    gender CHAR(1), -- Gender of the vendor
+    sales_region_id INT FOREIGN KEY REFERENCES sales_regions(id), -- ID of the sales region
+    created_at DATETIME DEFAULT GETDATE(), -- Date of registration
+    updated_at DATETIME DEFAULT GETDATE(), -- Date of last update
+    active BIT DEFAULT 1 -- Active flag for soft delete
 );
 
--- Tabla de regiones de ventas
+-- Table of sales regions
 CREATE TABLE sales_regions (
     id INT PRIMARY KEY IDENTITY,
-    country NVARCHAR(100),
-    regionName NVARCHAR(100),
-    createdAt DATETIME DEFAULT GETDATE(),
-    updatedAt DATETIME DEFAULT GETDATE(),
+    country NVARCHAR(100), -- Country
+    region_name NVARCHAR(100), -- Name of the region
+    created_at DATETIME DEFAULT GETDATE(), -- Date of registration
+    updated_at DATETIME DEFAULT GETDATE(), -- Date of last update
+    active BIT DEFAULT 1 -- Active flag for soft delete
 );
